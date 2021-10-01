92ebb9e3329ab573576bc794354d9f13595f0598
Megan V <elbowgrrl@gmail.com>
2021-10-01 11:09:58 -0700

    initial commit

--- components/Article.js
+++ components/Article.js
@@ -1,19 +1,26 @@
-import styles from '../styles/Article.module.css'
+import styles from "../styles/Article.module.css";
+import Image from "next/image";
 
 export default function Article() {
-
   return (
     <article className={styles.article}>
-      <header>
-      </header>
       <body>
-
+      <Image
+          alt="gallery image"
+          src="/img/landscape1.jpeg"
+          layout="fixed"
+          width="400"
+          height="300"
+        />
       </body>
-      <footer>
+      <footer className={styles.footer}>
         <span className={styles.span}>Nandhaka Pieris</span>
         <span className={styles.span}>2015-05-01</span>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
       </footer>
     </article>
-  )
+  );
 }
--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,17 @@
 import styles from '../styles/Header.module.css'
+import Image from "next/image"
 
 export default function Header() {
   return(
   <article className={styles.article}>
     <h1>James Thomas</h1>
+    <Image
+          alt="gallery image"
+          src="/img/profile.jpeg"
+          layout="fixed"
+          width="400"
+          height="600"
+        />
   </article>
   )
 }
