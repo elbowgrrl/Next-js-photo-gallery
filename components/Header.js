6143d59fd7a9807cfed763e21af0ca285a96a89f
Megan V <elbowgrrl@gmail.com>
2021-10-15 12:03:40 -0700

    completed header styling

--- components/Header.js
+++ components/Header.js
@@ -1,27 +1,31 @@
-import styles from '../styles/Header.module.css'
-import Image from "next/image"
+import styles from "../styles/Header.module.css";
+import Image from "next/image";
 
 export default function Header(props) {
-  return(
-  <article className={styles.article}>
-    <h1 className={styles.titleText}>{props.name}</h1>
-    {/* <img src="/img/profile.jpeg" alt="Photo of photographer" id="bio-photo"/> */}
-    <div style={{borderRadius: '150px', overflow: 'hidden', width: '300px',
-          height: '300px'}}>
-    
-    <Image
+  return (
+    <article className={styles.article}>
+      <div
+        style={{
+          borderRadius: "150px",
+          overflow: "hidden",
+          width: "300px",
+          height: "300px",
+        }}
+      >
+        <Image
           alt="gallery image"
           src="/img/profile.jpeg"
-          layout="responsive" 
+          layout="responsive"
           width="300"
           height="300"
         />
-        </div>
-        <p className={styles.bodyText}>{props.bio}</p>
-        
-        <span className={styles.bodyText}>{props.phone}</span>
-        <span className={styles.bodyText}>{props.email}</span>
-        
-  </article>
-  )
+      </div>
+      <div className={styles.infoContainer}>
+        <h1>{props.name}</h1>
+        <div>{props.bio}</div>
+        <div>{props.phone}</div>
+        <div>{props.email}</div>
+      </div>
+    </article>
+  );
 }
