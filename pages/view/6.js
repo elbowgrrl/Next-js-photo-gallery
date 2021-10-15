9ddb6060d31114e2a0a4543e53b5e66736d06747
Megan V <elbowgrrl@gmail.com>
2021-10-14 17:38:08 -0700

    adding view pages without using dynamic routes

---
+++ pages/view/6.js
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import data from "/data/landscapes.json";
+
+export async function getStaticProps() {
+
+  const allData = await data;
+
+  return {
+    props: {
+      allData,
+    },
+  };
+}
+
+const View = ({ allData }) => {
+
+  return (
+    <div>
+      <Image src={allData.album[5].img} width="640" height="320" />
+    </div>
+  );
+};
+
+export default View;
