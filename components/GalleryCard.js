c2d7e1eff0e9da4084b0df47a6462b5ce96757a7
Megan V <elbowgrrl@gmail.com>
2021-10-27 13:05:23 -0700

    more styling

--- components/GalleryCard.js
+++ components/GalleryCard.js
@@ -2,31 +2,34 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { Button, CardActions } from "@mui/material";
+import { Button, CardActionArea, CardActions } from "@mui/material";
 import styles from "../styles/Card.module.css";
 
 export default function GalleryCard(props) {
-
   return (
     <Card className={styles.card}>
-      <CardMedia className={styles.image}
-        component="img"
-        height="240"
-        image={props.img}
-        alt={props.title}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {props.title}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {props.description}
-        </Typography>
-      </CardContent>
+      <CardActionArea href={`/view/${props.id}`}>
+        <CardMedia
+          className={styles.image}
+          component="img"
+          height="240"
+          image={props.img}
+          alt={props.title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {props.title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {props.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
       <CardActions>
-        <Button size="small" variant="contained" href={`/view/${props.id}`}>View</Button>
+        <Button size="small" href={`/view/${props.id}`}>
+          View
+        </Button>
       </CardActions>
     </Card>
   );
 }
-
