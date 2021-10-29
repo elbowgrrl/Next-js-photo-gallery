d49badf9e5bc9607eb8079a58d15c69efad2755c
Megan V <elbowgrrl@gmail.com>
2021-10-29 09:58:42 -0700

    implomented conditional rendering of bio using media query

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,21 @@
 import styles from "../styles/Header.module.css";
 import { Paper } from "@mui/material";
+import { useMediaQuery } from '@material-ui/core';
+
+
 
 export default function Header(props) {
+
+  const bioCollapsed = useMediaQuery('(max-width: 600px)');
+  const bioFull = useMediaQuery('(min-width: 600px)');
 
   return (
     <Paper className={styles.article}>
       <img className={styles.image} src="/img/profile.jpeg" />
       <div className={styles.infoContainer}>
         <h1>{props.name}</h1>
-        <span>{props.bio}</span>
+        <div>{bioFull && <span>{props.bio}</span>}
+        </div>
         <span>{props.phone}</span>
         <span>{props.email}</span>
       </div>
