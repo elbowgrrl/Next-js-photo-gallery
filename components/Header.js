795a3eecaa98b4ec65345683ea7f8fcc83696f63
Megan V <elbowgrrl@gmail.com>
2021-10-29 10:51:28 -0700

    implimented conditionally rendered accordion to hold bio in mobile view only

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import styles from "../styles/Header.module.css";
-import { Paper } from "@mui/material";
+import { Paper, AccordionSummary, AccordionDetails, Accordion, Typography } from "@mui/material";
 import { useMediaQuery } from '@material-ui/core';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 
 
@@ -8,13 +9,29 @@ export default function Header(props) {
 
   const bioCollapsed = useMediaQuery('(max-width: 600px)');
   const bioFull = useMediaQuery('(min-width: 600px)');
+  const shortBio = props.bio.slice(0, 20) + '...'
 
   return (
     <Paper className={styles.article}>
       <img className={styles.image} src="/img/profile.jpeg" />
       <div className={styles.infoContainer}>
         <h1>{props.name}</h1>
-        <div>{bioFull && <span>{props.bio}</span>}
+        <div>
+          {bioFull && <span>{props.bio}</span>}
+          {bioCollapsed && 
+                <Accordion className={styles.accordion}>
+                <AccordionSummary
+                  expandIcon={<ArrowDropDownIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography>{shortBio}</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>{props.bio}</Typography>
+                </AccordionDetails>
+              </Accordion>
+          }
         </div>
         <span>{props.phone}</span>
         <span>{props.email}</span>
